8b5084ed94281adbd6d600cd85a46b6c99d6207c
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-05-13 00:13:59 +0330

    maybe fix celery config

--- manage_content_service/celery_app.py
+++ manage_content_service/celery_app.py
@@ -1,13 +1,14 @@
 import os
 from celery import Celery
-import celeryconfig
+# import celeryconfig
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manage_content_service.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manage_content_service.settings.development')
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manage_content_service.settings.base")
 print(os.environ.get('DJANGO_SETTINGS_MODULE'))
 print("hi")
+# print(celeryconfig)
 app = Celery()
 print('hii')
-app.config_from_object(celeryconfig, namespace="CELERY")
+app.config_from_object('django.conf:settings', namespace="CELERY")
 print('hijjj')
 app.autodiscover_tasks()
