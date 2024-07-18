4ca5f43fd1b685157ad285431ef3842c23a7919f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-07-18 23:52:04 +0330

    fix: increase URLField length

--- apps/fsm/models.py
+++ apps/fsm/models.py
@@ -983,7 +983,7 @@ class UploadFileAnswer(Answer):
     problem = models.ForeignKey('fsm.UploadFileProblem', null=True, blank=True, on_delete=models.PROTECT,
                                 related_name='answers')
     answer_file = models.FileField(upload_to='answers', max_length=4000, blank=False)
-    answer_file2 = models.URLField(blank=True)
+    answer_file2 = models.URLField(max_length=2000, blank=True)
 
     @property
     def string_answer(self):
