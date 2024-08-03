a540ee494853c3eaf7f057790f71bec1c9d04a93
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-04 02:21:07 +0330

    refactor: refactor a url name

--- apps/report/urls.py
+++ apps/report/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import get_user_excel, get_answer_excel
 
 urlpatterns = [
-    path('registration-form-participants/', get_user_excel,
-         name='registration_form_participants'),
+    path('registration-form-registrants/', get_user_excel,
+         name='registration_form_registrants'),
     path('registration-form-answers/', get_answer_excel, name='export_csv'),
 ]
