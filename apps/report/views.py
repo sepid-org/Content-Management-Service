0c54275e35d926ff798e943ec97bc5e8784a9cb6
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-04 19:26:43 +0330

    refactor: disable connecting to metabase in development

--- apps/report/views.py
+++ apps/report/views.py
@@ -5,40 +5,21 @@ from rest_framework.decorators import api_view
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from apps.file_storage.serializers.file_serializer import FileSerializer
+from proxies.metabase.main import MetabaseProxy
 
 url = settings.METABASE_URL
 
+Metabase_proxy = MetabaseProxy()
 
-def create_session():
+
+def get_user_answer(form_id):
     headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "username": settings.METABASE_USERNAME,
-        "password": settings.METABASE_PASSWORD,
-    }
-
-    response = requests.post(f'{url}api/session/', headers=headers, json=data)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        response.raise_for_status()
-
-
-session_data = create_session()
-
-
-def get_user_answer(session_token, form_id, user_id):
-    headers = {
-        "X-Metabase-Session": session_data.get("id"),
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = {
         "query": '{"database":2,"type":"query","query":{"source-table":60,"joins":[{"source-table":35,"condition":["=",["field",446,null],["field",271,{"join-alias":"Fsm+Answersheet+-+Answersheet+Ptr"}]],"alias":"Fsm+Answersheet+-+Answersheet+Ptr"},{"fields":[["field",264,{"join-alias":"Fsm+Answer"}]],"source-table":106,"condition":["=",["field",271,{"join-alias":"Fsm+Answersheet+-+Answersheet+Ptr"}],["field",269,{"join-alias":"Fsm+Answer"}]],"alias":"Fsm+Answer"},{"fields":[["field",290,{"join-alias":"Fsm+Biganswer"}]],"source-table":46,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",288,{"join-alias":"Fsm+Biganswer"}]],"alias":"Fsm+Biganswer"},{"fields":[["field",451,{"join-alias":"Fsm+Smallanswer"}]],"source-table":96,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",449,{"join-alias":"Fsm+Smallanswer"}]],"alias":"Fsm+Smallanswer"},{"fields":[["field",142,{"join-alias":"Accounts+User+-+User"}],["field",156,{"join-alias":"Accounts+User+-+User"}],["field",151,{"join-alias":"Accounts+User+-+User"}]],"source-table":32,"condition":["=",["field",443,null],["field",151,{"join-alias":"Accounts+User+-+User"}]],"alias":"Accounts+User+-+User"}],"filter":["and",["=",["field",779,null],'+f'{form_id}'+'],["=",["field",443,null],"'+f'{user_id}'+'"]]},"middleware":{"js-int-to-string?":true,"add-default-userland-constraints?":true}}'
     }
-    response = requests.post(
-        f'{url}api/dataset/xlsx', headers=headers, data=data)
+    response = Metabase_proxy.post(f'{url}api/dataset/xlsx', headers, data)
 
     if response.status_code == 200:
         with open('output.xlsx', 'wb') as f:
@@ -108,14 +89,12 @@ def get_current_user(session_token):
 
 def get_form_respondents_info_file(form_id):
     headers = {
-        "X-Metabase-Session": session_data.get("id"),
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = {
         "query": '{"database":2,"type":"query","query":{"source-table":60,"filter":["=",["field",779,null],' + f'{form_id}' + '],"joins":[{"fields":[["field",142,{"join-alias":"Accounts+User+-+User"}],["field",156,{"join-alias":"Accounts+User+-+User"}],["field",158,{"join-alias":"Accounts+User+-+User"}],["field",157,{"join-alias":"Accounts+User+-+User"}],["field",144,{"join-alias":"Accounts+User+-+User"}],["field",161,{"join-alias":"Accounts+User+-+User"}],["field",150,{"join-alias":"Accounts+User+-+User"}],["field",159,{"join-alias":"Accounts+User+-+User"}],["field",146,{"join-alias":"Accounts+User+-+User"}],["field",147,{"join-alias":"Accounts+User+-+User"}],["field",163,{"join-alias":"Accounts+User+-+User"}],["field",152,{"join-alias":"Accounts+User+-+User"}],["field",154,{"join-alias":"Accounts+User+-+User"}],["field",153,{"join-alias":"Accounts+User+-+User"}],["field",162,{"join-alias":"Accounts+User+-+User"}]],"source-table":32,"condition":["=",["field",443,null],["field",151,{"join-alias":"Accounts+User+-+User"}]],"alias":"Accounts+User+-+User"}],"fields":[["field",446,null],["field",444,null],["field",448,null],["field",447,null],["field",707,null]]},"middleware":{"js-int-to-string?":true,"add-default-userland-constraints?":true}}'
     }
-    response = requests.post(
-        f'{url}api/dataset/xlsx', headers=headers, data=data)
+    response = Metabase_proxy.post(f'{url}api/dataset/xlsx', headers, data)
 
     if response.status_code == 200:
         in_memory_file = SimpleUploadedFile("test.xlsx", response.content)
@@ -129,14 +108,12 @@ def get_form_respondents_info_file(form_id):
 
 def get_form_respondents_answers_file(form_id):
     headers = {
-        "X-Metabase-Session": session_data.get("id"),
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = {
         "query": '{"database":2,"type":"query","query":{"source-table":60,"joins":[{"fields":"none","source-table":35,"condition":["=",["field",446,null],["field",271,{"join-alias":"Fsm+Answersheet+-+Answersheet+Ptr"}]],"alias":"Fsm+Answersheet+-+Answersheet+Ptr"},{"fields":"none","source-table":106,"condition":["=",["field",271,{"join-alias":"Fsm+Answersheet+-+Answersheet+Ptr"}],["field",269,{"join-alias":"Fsm+Answer"}]],"alias":"Fsm+Answer"},{"fields":"none","source-table":96,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",449,{"join-alias":"Fsm+Smallanswer"}]],"alias":"Fsm+Smallanswer"},{"fields":"none","source-table":117,"condition":["=",["field",450,{"join-alias":"Fsm+Smallanswer"}],["field",430,{"join-alias":"Fsm+Problem+-+Problem"}]],"alias":"Fsm+Problem+-+Problem"},{"fields":"none","source-table":46,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",288,{"join-alias":"Fsm+Biganswer"}]],"alias":"Fsm+Biganswer"},{"fields":"none","source-table":107,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",466,{"join-alias":"Fsm+Uploadfileanswer"}]],"alias":"Fsm+Uploadfileanswer"},{"fields":"none","source-table":9,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",371,{"join-alias":"Fsm+Multichoiceanswer+Choices"}]],"alias":"Fsm+Multichoiceanswer+Choices"},{"fields":"none","source-table":66,"condition":["=",["field",372,{"join-alias":"Fsm+Multichoiceanswer+Choices"}],["field",300,{"join-alias":"Fsm+Choice+-+Choice"}]],"alias":"Fsm+Choice+-+Choice"},{"fields":"none","source-table":32,"condition":["=",["field",443,null],["field",151,{"join-alias":"Accounts+User+-+User"}]],"alias":"Accounts+User+-+User"}],"expressions":{"ans":["concat",["field",451,{"join-alias":"Fsm+Smallanswer"}],["field",290,{"join-alias":"Fsm+Biganswer"}],["field",467,{"join-alias":"Fsm+Uploadfileanswer"}],["field",303,{"join-alias":"Fsm+Choice+-+Choice"}]],"نام+و+نام+خانوادگی":["concat",["field",142,{"join-alias":"Accounts+User+-+User"}],"+",["field",156,{"join-alias":"Accounts+User+-+User"}]],"phone":["field",144,{"join-alias":"Accounts+User+-+User"}]},"fields":[["field",446,null],["expression","ans"],["expression","نام+و+نام+خانوادگی"],["expression","phone"]],"filter":["=",["field",779,null],'+f'{form_id}' + ']},"middleware":{"js-int-to-string?":true,"add-default-userland-constraints?":true}}'
     }
-    response = requests.post(
-        f'{url}api/dataset/xlsx', headers=headers, data=data)
+    response = Metabase_proxy.post(f'{url}api/dataset/xlsx', headers, data)
 
     if response.status_code == 200:
         in_memory_file = SimpleUploadedFile("test.xlsx", response.content)
---
+++ proxies/metabase/main.py
@@ -0,0 +1,34 @@
+import requests
+from django.conf import settings
+
+
+def create_session():
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "username": settings.METABASE_USERNAME,
+        "password": settings.METABASE_PASSWORD,
+    }
+
+    response = requests.post(f'{url}api/session/', headers=headers, json=data)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+class MetabaseProxy():
+
+    def __init__(self) -> None:
+        from django.conf import settings
+        if not settings.DEBUG:
+            self.session_data = create_session()
+
+    def post(self, url, headers, data):
+        headers = {
+            "X-Metabase-Session": self.session_data.get("id"),
+            **headers,
+        }
+        return requests.post(url=url, headers=headers, data=data)
