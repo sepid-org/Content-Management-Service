df50f9a8c59b7bdfa737b7df2b768eba77b87282
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-05 17:59:29 +0330

    refactor: remove redundant codes

--- apps/report/views.py
+++ apps/report/views.py
@@ -1,4 +1,3 @@
-import requests
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -10,81 +9,6 @@ from proxies.metabase.main import MetabaseProxy
 url = settings.METABASE_URL
 
 Metabase_proxy = MetabaseProxy()
-
-
-def get_user_answer(form_id):
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {
-        "query": '{"database":2,"type":"query","query":{"source-table":60,"joins":[{"source-table":35,"condition":["=",["field",446,null],["field",271,{"join-alias":"Fsm+Answersheet+-+Answersheet+Ptr"}]],"alias":"Fsm+Answersheet+-+Answersheet+Ptr"},{"fields":[["field",264,{"join-alias":"Fsm+Answer"}]],"source-table":106,"condition":["=",["field",271,{"join-alias":"Fsm+Answersheet+-+Answersheet+Ptr"}],["field",269,{"join-alias":"Fsm+Answer"}]],"alias":"Fsm+Answer"},{"fields":[["field",290,{"join-alias":"Fsm+Biganswer"}]],"source-table":46,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",288,{"join-alias":"Fsm+Biganswer"}]],"alias":"Fsm+Biganswer"},{"fields":[["field",451,{"join-alias":"Fsm+Smallanswer"}]],"source-table":96,"condition":["=",["field",263,{"join-alias":"Fsm+Answer"}],["field",449,{"join-alias":"Fsm+Smallanswer"}]],"alias":"Fsm+Smallanswer"},{"fields":[["field",142,{"join-alias":"Accounts+User+-+User"}],["field",156,{"join-alias":"Accounts+User+-+User"}],["field",151,{"join-alias":"Accounts+User+-+User"}]],"source-table":32,"condition":["=",["field",443,null],["field",151,{"join-alias":"Accounts+User+-+User"}]],"alias":"Accounts+User+-+User"}],"filter":["and",["=",["field",779,null],'+f'{form_id}'+'],["=",["field",443,null],"'+f'{user_id}'+'"]]},"middleware":{"js-int-to-string?":true,"add-default-userland-constraints?":true}}'
-    }
-    response = Metabase_proxy.post(f'{url}api/dataset/xlsx', headers, data)
-
-    if response.status_code == 200:
-        with open('output.xlsx', 'wb') as f:
-            f.write(response.content)
-
-        file = SimpleUploadedFile("test.xlsx", b"file_content")
-        new_file = FileSerializer(data={"file": file})
-        new_file.is_valid(raise_exception=True)
-        new_file.save()
-        print(new_file.data)
-        print("File created successfully:")
-        return new_file.data
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
-        print(response.text)
-
-
-def get_current_user(session_token):
-    headers = {
-        "Content-Type": "application/json",
-        "X-Metabase-Session": session_token
-    }
-    data = {
-        "database": 2,
-        "query": {
-            "source-table": 60,
-            "filter": [
-                "and",
-                ["=", ["field", 448, None], True],
-                ["=", ["field", 779, None], 1]
-            ],
-            "joins": [
-                {
-                    "fields": [
-                        ["field", 142, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 156, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 158, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 144, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 159, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 151, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 146, {
-                            "join-alias": "Accounts+User+-+User"}],
-                        ["field", 147, {
-                            "join-alias": "Accounts+User+-+User"}]
-                    ],
-                    "source-table": 32,
-                    "condition": ["=", ["field", 443, None],
-                                  ["field", 151, {"join-alias": "Accounts+User+-+User"}]],
-                    "alias": "Accounts+User+-+User"
-                }
-            ]
-        },
-    }
-
-    response = requests.post(f'{url}api/dataset/', headers=headers, json=data)
-    if response.status_code == 202:
-        return response.json()
-    else:
-        response.raise_for_status()
 
 
 def get_form_respondents_info_file(form_id):
