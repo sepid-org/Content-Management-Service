fcdf3188bf75b55c996d2f50a6cf20d396116620
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-10 05:05:46 +0330

    refactor: separate sales models admins from accounts models admins

--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from apps.accounts.models import Purchase, DiscountCode, Merchandise, Voucher
+
+
+@admin.register(Merchandise)
+class MerchandiseCustomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'program',
+                    'price', 'discounted_price', 'is_active', 'is_deleted']
+    list_filter = ['is_active', 'program']
+
+
+@admin.register(Purchase)
+class CustomPurchaseAdmin(admin.ModelAdmin):
+    model = Purchase
+    list_display = ['id', 'ref_id', 'amount',
+                    'status', 'created_at', 'user', 'merchandise']
+    search_fields = ['user__username']
+
+
+admin.site.register(Voucher)
+admin.site.register(DiscountCode)
