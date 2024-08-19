3093103526d943b8de476ee80b7aac09fded25c1
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-19 23:20:35 +0330

    refactor: remove redundant import

--- apps/fsm/serializers/widgets/widget_serializer.py
+++ apps/fsm/serializers/widgets/widget_serializer.py
@@ -47,7 +47,6 @@ class WidgetSerializer(serializers.ModelSerializer):
         return super(WidgetSerializer, self).validate(attrs)
 
     def to_representation(self, instance):
-        from apps.response.serializers.answers.answer_polymorphic_serializer import AnswerPolymorphicSerializer
         representation = super(
             WidgetSerializer, self).to_representation(instance)
         if 'solution' in representation.keys() and instance.paper.is_exam:
