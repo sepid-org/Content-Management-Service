a2b1fa2547f2a0938d274f99e7e4ff313ce859ab
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-23 22:33:16 +0330

    fix: fix sending program_slug instead of program_id

--- apps/sales/views/merchandise_view.py
+++ apps/sales/views/merchandise_view.py
@@ -46,8 +46,8 @@ class MerchandiseViewSet(ModelViewSet):
     @transaction.atomic
     @action(detail=False, methods=['GET'])
     def program_merchandises(self, request, pk=None):
-        program_id = request.GET.get('program', None)
-        merchandises = self.get_queryset().filter(program_id=program_id)
+        program_slug = request.GET.get('program_slug', None)
+        merchandises = self.get_queryset().filter(program_slug=program_slug)
         return Response(self.serializer_class(merchandises, many=True).data, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['get'])
