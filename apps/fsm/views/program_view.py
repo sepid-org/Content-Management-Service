b062c58e9eff913c9685bbc215d98e091af0464b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-30 13:14:47 +0330

    refactor: refactor program viewset

--- apps/fsm/views/program_view.py
+++ apps/fsm/views/program_view.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.utils.decorators import method_decorator
 from django.core.cache import cache
 
@@ -21,22 +21,17 @@ from utilities.safe_auth import SafeTokenAuthentication
 
 
 class ProgramViewSet(ModelViewSet):
-    serializer_class = ProgramSerializer
     queryset = Program.objects.filter(is_deleted=False)
-    my_tags = ['program']
-    filterset_fields = ['website']
+    serializer_class = ProgramSerializer
     pagination_class = ProgramsPagination
     authentication_classes = [SafeTokenAuthentication]
     permission_classes = [IsAuthenticated]
+    filterset_fields = ['website']
+    lookup_field = 'slug'
 
     def dispatch(self, request, *args, **kwargs):
         response = super().dispatch(request, *args, **kwargs)
-
-        # Add headers to prevent frontend caching
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
-
+        self._add_no_cache_headers(response)
         return response
 
     def get_object(self):
@@ -49,8 +44,8 @@ class ProgramViewSet(ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['retrieve', 'list']:
-            return [IsAuthenticated()]
-        return super().get_permissions()
+            return [AllowAny()]
+        return [ProgramAdminPermission()]
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -59,56 +54,43 @@ class ProgramViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context.update({'user': self.request.user})
+        context['user'] = self.request.user
         return context
 
-    def get_permissions(self):
-        if self.action in ['retrieve', 'list']:
-            permission_classes = [permissions.AllowAny]
-        else:
-            permission_classes = [ProgramAdminPermission]
-        return [permission() for permission in permission_classes]
-
     @action(detail=True, methods=['get'])
-    def get_admins(self, request, pk=None):
+    def get_admins(self, request, slug=None):
         program = self.get_object()
         serializer = UserSerializer(program.admins.all(), many=True)
         return Response(serializer.data)
 
     @action(detail=True, methods=['post'])
-    def add_admin(self, request, pk=None):
+    def add_admin(self, request, slug=None):
         program = self.get_object()
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = find_user_in_website(
-            user_data=serializer.validated_data,
-            website=request.data.get("website"),
-            raise_exception=True,
-        )
+        user = self._find_user(serializer.validated_data,
+                               request.data.get("website"))
         add_admin_to_program(user, program)
         return Response(status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'], serializer_class=UserSerializer)
-    def remove_admin(self, request, pk=None):
+    @action(detail=True, methods=['post'])
+    def remove_admin(self, request, slug=None):
         program = self.get_object()
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        removed_admin = find_user_in_website(
-            user_data=serializer.validated_data,
-            website=request.data.get("website")
-        )
-        if removed_admin == program.creator:
-            raise ParseError(serialize_error('5007'))
+        removed_admin = self._find_user(
+            serializer.validated_data, request.data.get("website"))
+        self._ensure_not_creator(removed_admin, program)
         program.admins.remove(removed_admin)
         return Response(status=status.HTTP_200_OK)
 
     @action(detail=True, methods=['get'])
-    def soft_delete(self, request, pk=None):
+    def soft_delete(self, request, slug=None):
         program = self.get_object()
         program.is_deleted = True
         program.deleted_at = timezone.now()
         program.save()
-        self.invalidate_list_cache()
+        self._invalidate_list_cache()
         return Response()
 
     @method_decorator(custom_cache_page(60 * 15, cache_key_prefix='programs_list'))
@@ -117,19 +99,30 @@ class ProgramViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        self.invalidate_list_cache()
+        self._invalidate_list_cache()
         return response
 
     def update(self, request, *args, **kwargs):
         response = super().update(request, *args, **kwargs)
-        self.invalidate_list_cache()
+        self._invalidate_list_cache()
         return response
 
     def partial_update(self, request, *args, **kwargs):
         response = super().partial_update(request, *args, **kwargs)
-        self.invalidate_list_cache()
+        self._invalidate_list_cache()
         return response
 
-    def invalidate_list_cache(self):
-        # Invalidate cache for all possible list URLs
+    def _add_no_cache_headers(self, response):
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+
+    def _find_user(self, user_data, website):
+        return find_user_in_website(user_data=user_data, website=website, raise_exception=True)
+
+    def _ensure_not_creator(self, user, program):
+        if user == program.creator:
+            raise ParseError(serialize_error('5007'))
+
+    def _invalidate_list_cache(self):
         cache.delete_pattern('programs_list:*')
