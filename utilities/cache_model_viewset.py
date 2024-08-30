caca24cda7f831a5bbaf8894d0d71c8c77105132
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-08-30 13:49:28 +0330

    fix: fix a bug in CacheModelViewSet

--- utilities/cache_model_viewset.py
+++ utilities/cache_model_viewset.py
@@ -1,36 +1,26 @@
 from rest_framework.viewsets import ModelViewSet
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.core.cache import cache
-from django.utils.functional import classproperty
+from django.utils.encoding import iri_to_uri
 
 
 class CacheModelViewSet(ModelViewSet):
-    # Configuration options with defaults
     cache_timeout = 60 * 15  # 15 minutes
+    cache_list_only = True
     add_no_cache_headers = True
 
-    @classproperty
-    def cache_key_prefix(cls):
-        return f'{cls.__name__.lower()}'
+    def get_cache_key(self, request):
+        return f"viewset-{self.__class__.__name__}-list-{iri_to_uri(request.get_full_path())}"
 
-    def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-        if self.add_no_cache_headers:
-            self._add_no_cache_headers(response)
-        return response
-
-    def _cached_list(self, request, *args, **kwargs):
-        cache_key = f"{self.cache_key_prefix}:list:{request.get_full_path()}"
-        cached_response = cache.get(cache_key)
-
-        if cached_response is None:
-            response = super().list(request, *args, **kwargs)
-            cache.set(cache_key, response, self.cache_timeout)
-            return response
-
-        return cached_response
-
+    @method_decorator(cache_page(cache_timeout))
     def list(self, request, *args, **kwargs):
-        return self._cached_list(request, *args, **kwargs)
+        return super().list(request, *args, **kwargs)
+
+    def _invalidate_list_cache(self):
+        # Instead of using delete_pattern, we'll delete the specific key
+        cache_key = self.get_cache_key(self.request)
+        cache.delete(cache_key)
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
@@ -47,10 +37,18 @@ class CacheModelViewSet(ModelViewSet):
         self._invalidate_list_cache()
         return response
 
+    def destroy(self, request, *args, **kwargs):
+        response = super().destroy(request, *args, **kwargs)
+        self._invalidate_list_cache()
+        return response
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        if self.add_no_cache_headers:
+            self._add_no_cache_headers(response)
+        return response
+
     def _add_no_cache_headers(self, response):
         response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
         response['Pragma'] = 'no-cache'
         response['Expires'] = '0'
-
-    def _invalidate_list_cache(self):
-        cache.delete_pattern(f'{self.cache_key_prefix}:list:*')
