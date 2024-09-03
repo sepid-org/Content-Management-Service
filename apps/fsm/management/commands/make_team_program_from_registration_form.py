1c959a7be2669eb9aad6f2690bd88b3224d2bf9f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-09-03 15:42:17 +0330

    refactor: remove registration_form field from team

--- apps/fsm/management/commands/make_team_program_from_registration_form.py
+++ apps/fsm/management/commands/make_team_program_from_registration_form.py
@@ -6,6 +6,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for team in Team.objects.all():
-            if team.registration_form:
-                team.program = team.registration_form.program
+            # if team.registration_form:
+            #     team.program = team.registration_form.program
+            #     team.save()
+            #     continue
+
+            members = team.members.all()
+            if len(members) > 0:
+                first_member = members[0]
+                team.program = first_member.form.program
                 team.save()
+                continue
