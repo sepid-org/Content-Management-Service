180acc62edb35286510a536a8c229697384e74ee
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2024-10-02 19:54:04 +0330

    fix urls

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import get_form_respondents_info, get_form_respondents_answers, get_program_merchandises_purchases , get_form_id_give_answer
+from .views import get_form_respondents_info, get_program_merchandises_purchases , get_form_id_give_answer
 
 urlpatterns = [
     path('form-respondents-info/', get_form_respondents_info,
          name='form_respondents_info'),
-    path('hi/', get_form_id_give_answer),
-    path('form-respondents-answers/',
-         get_form_respondents_answers, name='form_respondents_answers'),
+    path('form-respondents-answers/', get_form_id_give_answer),
     path('program-merchandises-purchases/',
          get_program_merchandises_purchases, name='program_merchandises_purchases'),
 ]
