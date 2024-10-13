ddbbb77ce94369db312ca6e4ede14da1854e93c6
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-10-14 01:48:56 +0330

    fix: fix a bug (when object has no position)

--- apps/fsm/serializers/object_serializer.py
+++ apps/fsm/serializers/object_serializer.py
@@ -2,11 +2,24 @@
 from rest_framework import serializers
 
 from apps.fsm.models import Object
+from apps.fsm.models.base import Position
 from apps.fsm.serializers.position_serializer import PositionSerializer
 
 
 class ObjectSerializer(serializers.ModelSerializer):
-    position = PositionSerializer(required=False, read_only=True)
+    position = serializers.SerializerMethodField(
+        required=False, read_only=True)
+
+    def get_position(self, obj):
+        if not hasattr(obj, 'position'):
+            Position.objects.create(
+                object=obj,
+                x=0,  # Default x position
+                y=0,  # Default y position
+                width=300,  # Default width
+                height=200  # Default height
+            )
+        return PositionSerializer(obj.position).data
 
     class Meta:
         model = Object
