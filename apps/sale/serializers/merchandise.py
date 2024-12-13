b45733912634efbd1c8b6925aad75ea9ec3d1d7a
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-12-13 13:40:14 +0330

    fix: fix creating merchandise

--- apps/sale/serializers/merchandise.py
+++ apps/sale/serializers/merchandise.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
 from apps.accounts.models import Merchandise
+from apps.fsm.models.program import Program
 from apps.sale.validators import price_validator
 
 
 class MerchandiseSerializer(serializers.ModelSerializer):
+    program = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Program.objects.all(),
+        required=True
+    )
     price = serializers.IntegerField(
         required=True, validators=[price_validator])
     discounted_price = serializers.IntegerField(
@@ -12,5 +18,6 @@ class MerchandiseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Merchandise
-        fields = ['id', 'name', 'price', 'discounted_price', 'is_active', 'program', 'is_deleted']
+        fields = ['id', 'name', 'price', 'discounted_price',
+                  'is_active', 'program', 'is_deleted']
         read_only_fields = ['id']
