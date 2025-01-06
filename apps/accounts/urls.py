b8edb177d87ceddb6dd70a7be357357e13e13d4d
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-01-06 20:46:26 +0330

    feat: add an api for checking authentication (is user expired or not)

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.accounts.views.change_password import ChangePasswordView
 from apps.accounts.views.change_phone_number import change_phone_number_view
+from apps.accounts.views.check_auth import CheckAuthenticationView
 from apps.accounts.views.google_login import GoogleLogin
 from apps.accounts.views.otp_login import OTPLoginView
 from apps.accounts.views.simple_login import SimpleLogin
@@ -23,6 +24,10 @@ router.register(r'profile', ProfileViewSet, basename='profiles')
 router.register(r'studentship', StudentshipViewSet, basename='studentships')
 
 urlpatterns = [
+    path('accounts/check-authentication/',
+         CheckAuthenticationView.as_view(), name='check-authentication'),
+    path('accounts/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+
     path('accounts/simple-login/', SimpleLogin.as_view(), name='simple-login'),
     path('accounts/otp-login/', OTPLoginView.as_view(), name='otp-login'),
     path("accounts/google-login/", GoogleLogin.as_view(), name="google-login"),
@@ -30,7 +35,6 @@ urlpatterns = [
 
     path('accounts/verification-code/', VerificationCodeView.as_view(),
          name="send-verification-code"),
-    path('accounts/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('accounts/change-password/',
          ChangePasswordView.as_view(), name="change-password"),
     path("accounts/change-phone-number/",
