658abc43a6aa4fde86ecc2bd8d41017977d06b97
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-01-13 18:02:17 +0330

    Temp: remove get_user_data_from_shad

--- apps/accounts/views/uuid_login.py
+++ apps/accounts/views/uuid_login.py
@@ -76,14 +76,15 @@ class UserIDLoginView(BaseLoginView):
                 user.save()
             response_status = status.HTTP_201_CREATED
 
-        try:
-            user_data = get_user_data_from_shad(
-                user_uuid=user_identifier,
-                landing_id=landing_id,
-            )
-            update_user_info_by_shad_data(user, user_data)
-        except ValueError as e:
-            raise Exception('cant get data from Shad')
+        # try:
+        #     user_data = get_user_data_from_shad(
+        #         user_uuid=user_identifier,
+        #         landing_id=landing_id,
+        #     )
+        #     update_user_info_by_shad_data(user, user_data)
+        # except ValueError as e:
+        #     pass
+        #     # raise Exception('cant get data from Shad')
 
         self.create_login_event(user, website)
         return self.generate_response(user, created, response_status)
