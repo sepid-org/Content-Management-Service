d3d5e3d5f504a6edc29738bf30ab6fe9bc78be13
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-01-22 00:22:34 +0330

    Refactor: remove redundant enters!

--- apps/fsm/models/certificate.py
+++ apps/fsm/models/certificate.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 from apps.fsm.models.form import RegistrationForm
--- apps/fsm/models/content_widgets.py
+++ apps/fsm/models/content_widgets.py
@@ -1,5 +1,3 @@
-
-
 from django.db import models
 
 from apps.fsm.models.base import Paper, Widget, clone_widget
