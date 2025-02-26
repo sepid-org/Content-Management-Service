8ad3a2fc51059e7a98544edb6683756229a6e10d
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-02-26 21:38:14 +0330

    Fix: increase studentship_type max_length

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -32,7 +32,7 @@ class User(AbstractUser):
         max_length=15, blank=True, null=True, unique=True
     )
     # national code should not be unique, due it's not validated
-    national_code = models.CharField(max_length=100, null=True, blank=True)
+    national_code = models.CharField(max_length=10, null=True, blank=True)
     profile_image = models.URLField(blank=True, null=True, max_length=2000)
     bio = models.CharField(max_length=300, blank=True, null=True)
     gender = models.CharField(
@@ -188,8 +188,7 @@ class Studentship(PolymorphicModel):
         Academic = "Academic"
 
     studentship_type = models.CharField(
-        max_length=10, null=False, blank=False, choices=StudentshipType.choices
-    )
+        max_length=100, null=False, blank=False, choices=StudentshipType.choices)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     document = models.URLField(max_length=2000, null=True)
