653a4084711c2506e8afbb691b4ef44ce89f9050
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-03-01 19:58:01 +0330

    Feat: migrate static files to Arvan

--- content_management_service/utils/storages.py
+++ content_management_service/utils/storages.py
@@ -4,3 +4,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class PublicS3Storage(S3Boto3Storage):
     default_acl = 'public-read'  # Set ACL to public-read
     file_overwrite = False  # Prevent overwriting files with the same name
+
+
+class StaticS3Storage(S3Boto3Storage):
+    default_acl = 'public-read'
+    file_overwrite = False
+    location = 'static'
