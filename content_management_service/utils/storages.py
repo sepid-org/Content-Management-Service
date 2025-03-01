5d667477cdcfb0115b185d66f984327c1a005f69
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-03-01 15:56:29 +0330

    Feat: connect CMS file storage to Arvan

---
+++ content_management_service/utils/storages.py
@@ -0,0 +1,6 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class PublicS3Storage(S3Boto3Storage):
+    default_acl = 'public-read'  # Set ACL to public-read
+    file_overwrite = False  # Prevent overwriting files with the same name
