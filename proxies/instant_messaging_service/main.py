25e121b847e5f9454698132f8929f8a1e6f71e8f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-04-23 18:32:19 +0330

    Refactor: change user_id with user in InstantMessagingServiceProxy

--- proxies/instant_messaging_service/main.py
+++ proxies/instant_messaging_service/main.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 import requests
-from apps.accounts.models import User
 from utils.singleton_class import Singleton
 
 
@@ -10,19 +9,19 @@ class InstantMessagingServiceProxy(Singleton):
     def __init__(self, website):
         self.website = website
 
-    def send_solve_question_reward_notification(self, recipient, question_id, cost):
+    def send_solve_question_reward_notification(self, recipient_id, question_id, cost):
         message = f'بابت حل سوال {question_id} به شما {cost} امتیاز اضافه شد'
-        self._send_notification(recipient=recipient, message=message)
+        self._send_notification(recipient_id=recipient_id, message=message)
 
-    def send_submit_answer_cost_notification(self, recipient, question_id, cost):
+    def send_submit_answer_cost_notification(self, recipient_id, question_id, cost):
         message = f'بابت اقدام به حل سوال {question_id} {cost} قدر پول از شما کم شد'
-        self._send_notification(recipient=recipient, message=message)
+        self._send_notification(recipient_id=recipient_id, message=message)
 
-    def send_greeting_notification(self, recipient):
+    def send_greeting_notification(self, recipient_id):
         message = 'به آکادمی خود خوش آمدید!'
-        self._send_notification(recipient=recipient, message=message)
+        self._send_notification(recipient_id=recipient_id, message=message)
 
-    def _send_notification(self, recipient: User, message):
+    def _send_notification(self, recipient_id, message):
         res = requests.post(
-            f'{self.url}/send-message', json={'sender': self.website, 'recipient': str(recipient.id), 'message': message})
+            f'{self.url}/send-message', json={'sender': self.website, 'recipient': str(recipient_id), 'message': message})
         return res.status_code
