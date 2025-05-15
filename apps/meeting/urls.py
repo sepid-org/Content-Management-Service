1e40b95674f2b2f27804373e75c4fce950e6561f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-05-15 21:50:45 +0330

    Temp: remove Meeting model

--- apps/meeting/urls.py
+++ apps/meeting/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.meeting.views.meeting import MeetingViewSet
 
 router = DefaultRouter()
 
-router.register(r'meetings', MeetingViewSet, basename='meeting')
+# router.register(r'meetings', MeetingViewSet, basename='meeting')
 
 urlpatterns = [
     path('', include(router.urls)),
